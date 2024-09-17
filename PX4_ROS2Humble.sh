4c2fe4e11c663fe1fe79f4fb7b9deb0f79bef672
Bryan Perez <61372055+bperez134@users.noreply.github.com>
2024-09-16 21:12:48 -0700

    Create PX4_ROS2Humble.sh

---
+++ PX4_ROS2Humble.sh
@@ -0,0 +1,26 @@
+echo "Install PX4"
+
+cd
+git clone https://github.com/PX4/PX4-Autopilot.git --recursive
+bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
+cd PX4-Autopilot/
+make px4_sitl
+
+echo "Install ROS 2 Humble Hawksbill"
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt upgrade -y
+sudo apt install ros-humble-ros-base
+sudo apt install ros-dev-tools
+source /opt/ros/humble/setup.bash && echo "source /opt/ros/humble/setup.bash" >> .bashrc
+
+echo "Python Dependencies"
+pip install --user -U empy==3.3.4 pyros-genmsg setuptools
