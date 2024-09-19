4e8a771331862da26d3c638d0aeca2913122c784
Bryan Perez <61372055+bperez134@users.noreply.github.com>
2024-09-18 18:09:21 -0700

    Create AgentSetup.sh
    chmod +x <the script>
    ./<the script>

---
+++ AgentSetup.sh
@@ -0,0 +1,10 @@
+echo "Agent Setup"
+
+git clone https://github.com/eProsima/Micro-XRCE-DDS-Agent.git
+cd Micro-XRCE-DDS-Agent
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig /usr/local/lib/
