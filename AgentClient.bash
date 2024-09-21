f9e3e057063a95e8c6602bf7ea61729503a84fe0
Bryan Perez <61372055+bperez134@users.noreply.github.com>
2024-09-21 10:44:48 -0700

    Create AgentClient.bash
    Starting the Micro-XRCE-DOS-Agent and Client

---
+++ AgentClient.bash
@@ -0,0 +1,10 @@
+echo "Starting Agent and Client Communication"
+
+echo "Opening new terminal to start the agent"
+
+gnome-terminal -- bash -c "echo 'Starting Agent'; cd Micro-XRCE-DOS-Agent; cd build; MicroXRCEAgent udp4 -p 8888; exec bash"
+
+echo "Opening new terminal to start the client" 
+
+gnome-terminal -- bash -c "echo 'Starting Client'; cd Micro-XRCE-DOC-Agent; cd build; make px4_sitl gz_x500; exec bash"
+
