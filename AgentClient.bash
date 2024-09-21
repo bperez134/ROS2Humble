31d071ba6ec4cc7ac4ea6f7c6640083afd739d8b
Bryan Perez <61372055+bperez134@users.noreply.github.com>
2024-09-21 10:56:13 -0700

    Update AgentClient.bash

--- AgentClient.bash
+++ AgentClient.bash
@@ -2,9 +2,9 @@ echo "Starting Agent and Client Communication"
 
 echo "Opening new terminal to start the agent"
 
-gnome-terminal -- bash -c "echo 'Starting Agent'; cd Micro-XRCE-DOS-Agent; cd build; MicroXRCEAgent udp4 -p 8888; exec bash"
+gnome-terminal -- bash -c "echo 'Starting Agent'; cd Micro-XRCE-DDS-Agent; cd build; MicroXRCEAgent udp4 -p 8888; exec bash"
 
 echo "Opening new terminal to start the client" 
 
-gnome-terminal -- bash -c "echo 'Starting Client'; cd Micro-XRCE-DOS-Agent; cd build; make px4_sitl gz_x500; exec bash"
+gnome-terminal -- bash -c "echo 'Starting Client'; cd Micro-XRCE-DDS-Agent; cd build; make px4_sitl gz_x500; exec bash"
 
