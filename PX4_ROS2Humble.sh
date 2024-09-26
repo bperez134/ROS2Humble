0363ee361062e143d69ccc0a2ecba947bcd65026
Bryan Perez <61372055+bperez134@users.noreply.github.com>
2024-09-26 15:21:15 -0700

    Update PX4_ROS2Humble.sh

--- PX4_ROS2Humble.sh
+++ PX4_ROS2Humble.sh
@@ -19,6 +19,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update && sudo apt upgrade -y
 sudo apt install ros-humble-ros-base
+sudo apt install ros-humble-desktop
 sudo apt install ros-dev-tools
 source /opt/ros/humble/setup.bash && echo "source /opt/ros/humble/setup.bash" >> .bashrc
 
