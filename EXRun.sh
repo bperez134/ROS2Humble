4581cf1c653aae5dfa985d12758a84267cc9193e
Bryan Perez <61372055+bperez134@users.noreply.github.com>
2024-10-21 11:54:21 -0700

    Delete EXRun.sh

--- EXRun.sh
+++
@@ -1,8 +0,0 @@
-echo "Running the Example"
-
-cd ~/ws_sensor_combined/
-source /opt/ros/humble/setup.bash
-
-source install/local_setup.bash
-
-ros2 launch px4_ros_com sensor_combined_listener.launch.py
